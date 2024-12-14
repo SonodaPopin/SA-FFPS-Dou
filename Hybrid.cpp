efd2f55dcf9ec392fc5f6cad67889860e9d36f0c
Hugo Flores <hflores2022@udec.cl>
2024-12-14 17:32:14 -0300

    cruzador

--- Hybrid.cpp
+++ Hybrid.cpp
@@ -16,12 +16,50 @@ bool Hybrid::checkTime() {
     return duration_cast<seconds>(tiempoActual - startTime).count() >= maxTime;
 }
 
+void Hybrid::selecMejores(int mejor) {
+    for (int i = 0; i < valoresN.size(); i++) {
+        if (valoresN[i] = mejor){
+            valoresTOP.push_back(valoresN[i]);
+            cadenasTOP.push_back(cadenasN[i]);
+            valoresN.erase(valoresN.begin() + i);
+            cadenasN.erase(cadenasN.begin() + i);
+        }
+    }
+}
+
+void Hybrid::cruzador(){
+    for (int i = 0; i < cadenasN.size(); i++) {
+        //hijo = sexo(cadenasN[i], cadenasTOP[i % cadenasTOP.size()])
+        //cadenasN[i] = hijo;
+        //valoresN[i] = contarDiferencias(cadenasN[i], cadenasOriginales, thr);
+    }
+}
+
+void Hybrid::genetizar() {
+    cerr << "genetizando" << endl;
+    startTime = system_clock::now();
+    while (!checkTime()) {
+        int maxValue = *std::max_element(valoresN.begin(), valoresN.end());
+        cerr << "mejor obtenida de generacion " << maxValue << endl;
+        selecMejores(maxValue);
+        //matador();
+        if (maxValue > solQuality) {
+            solQuality = maxValue;
+            bestTime = system_clock::now();
+            cout << "Mejor calidad obtenida: " << solQuality 
+                 << " Tiempo usado para obtenerla: " << getFinalTime() << " segundos." << endl;
+        }
+    }
+}
+
 Hybrid::Hybrid(const std::string& ifp, int maxTime, int sizeN, float thr, float alpha) 
     : maxTime(maxTime), cadenasOriginales(leerArchivo(ifp)), thr(thr), codicia(ifp, alpha){
     cadenasN.resize(sizeN);
     valoresN.resize(sizeN);
+    cadenasTOP.resize(sizeN);
+    valoresTOP.resize(sizeN);
     cadenasN = codicia.generarSolucion(sizeN);
-    for (int i = 0; i < 100; i++) {
+    for (int i = 0; i < sizeN; i++) {
         valoresN[i] = contarDiferencias(cadenasN[i], cadenasOriginales, thr);
     }
 }
--- Hybrid.h
+++ Hybrid.h
@@ -12,7 +12,7 @@
 
 class Hybrid {
 private:
-    float thr = 0.8;                         ///< Umbral para determinar diferencias
+    float thr;                         ///< Umbral para determinar diferencias
     int maxTime;                             ///< Tiempo máximo permitido para la ejecución
     int solQuality = 0;                      ///< Calidad de la mejor solución encontrada
     AGreedy2 codicia;
@@ -20,11 +20,16 @@ private:
     std::vector<std::string> cadenasOriginales; ///< Cadenas originales leídas del archivo
     std::vector<std::string> cadenasN;    ///< Población actual de soluciones
     std::vector<int> valoresN;            ///< Calidad de las soluciones en `cadenas100`
+    std::vector<std::string> cadenasTOP;    ///< Población actual de soluciones
+    std::vector<int> valoresTOP;            ///< Calidad de las soluciones en `cadenas100`
     std::chrono::system_clock::time_point startTime; ///< Tiempo de inicio del algoritmo
     std::chrono::system_clock::time_point bestTime = std::chrono::system_clock::time_point::min(); ///< Tiempo de la mejor solución
     std::mt19937 gen;                        ///< Generador de números aleatorios
 
     bool checkTime();                        ///< Verifica si el tiempo máximo ha sido alcanzado
+    void Hybrid::selecMejores(int mejor);
+    void Hybrid::cruzador();
+    void Hybrid::genetizar();
 
 public:
     /**
